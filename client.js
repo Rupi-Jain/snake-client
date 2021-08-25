8795d5abdfcf587f2f3fc25810cb44e190d33269
Rupi-Jain <rupi.jain@gmail.com>
2021-08-25 19:28:09 -0400

    Third commit

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require("net");
 
-// establishes a connection with the game server
+//establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133',// IP address here,
@@ -11,9 +11,15 @@ const connect = function () {
   //messgae after connection to the server has been established
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write("Move: up");
-    conn.write("Rup: hey");
+    conn.write("Name: Rup");
   })
+
+  conn.on('connect' , () => {
+    setInterval( () => {
+      conn.write("Move: right");    
+    }, 1000)
+  })
+
   // interpret incoming data from server as text
   conn.on("data", (data) => {
     console.log(data);
